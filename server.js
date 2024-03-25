a49481ba4c02a38b9c392c20cefd2b997dde1689
Thomas <t.w.youens2@newcastle.ac.uk>
2024-03-25 21:46:13 +0000

    add connection to DB

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const config = dotenv.config();
 dotenvExpand.expand(config);
 
 const apiRouter = require("./src/routes/api.v1");
+const connect = require('./src/services/db.service');
 
 app.use("/api/v1", apiRouter);
-
+connect();
 app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: ${process.env.APP_URL}`)})
