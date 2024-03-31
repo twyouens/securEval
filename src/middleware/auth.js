ab67c288bf69addfd408efdd0f07987bb25d9e2f
Thomas <t.w.youens2@newcastle.ac.uk>
2024-03-31 19:46:18 +0100

    added logging methods

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,29 @@
 const APIKey = require('../models/apiKey.model');
+const logger = require('../services/logger.service');
 
 async function checkAPIKey(req, res, next) {
-
     if(!req.headers.authorization){
+        req.log.warn({err: "Unauthorized Request", detail: "No Authorization Header"}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
     const token = (req.headers.authorization || "").replace(/^Bearer\s/,"");
     const apiKey = await APIKey.findOne({key: token}, function(err, apiKey){
         if(err){
+            req.log.error({err: err, detail: "Error finding API Key"}, "Internal Server Error");
             res.status(500).json({message: 'Internal Server Error'});
             return false;
         }
     });
     if(!apiKey){
+        req.log.warn({err: "Unauthorized Request", detail: "API Key not found"}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
     if(apiKey.key !== token){
+        req.log.warn({err: "Unauthorized Request", detail: "API Key does not match", data: {apiKey: apiKey}}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
     if(!apiKey.isActive){
+        req.log.warn({err: "Unauthorized Request", detail: "API Key is not active", data: {apiKey: apiKey}}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
     req.token = apiKey;
