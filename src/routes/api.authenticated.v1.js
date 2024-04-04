348e8e85bc3f431ba76b275f7b58e6def224f1b6
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-04 10:32:27 +0100

    add api routes for tenant, users and rules

--- src/routes/api.authenticated.v1.js
+++ src/routes/api.authenticated.v1.js
@@ -5,8 +5,23 @@ const { checkAPIKey } = require("../middleware/auth");
 const apiAuthenticatedRouter = express.Router();
 apiAuthenticatedRouter.use(checkAPIKey);
 
+
+//rules
 apiAuthenticatedRouter.get("/rules", apiAuthenticatedController.getRules);
+apiAuthenticatedRouter.post("/rule", apiAuthenticatedController.createRule);
 apiAuthenticatedRouter.get("/rule/:ruleID", apiAuthenticatedController.getRule);
 apiAuthenticatedRouter.post("/rule/:ruleID/evaluate", apiAuthenticatedController.evaluateRule);
+
+//tenants
+apiAuthenticatedRouter.get("/tenant/me", apiAuthenticatedController.getMyTenant);
+
+//users
+// apiAuthenticatedRouter.get("/user/me", apiAuthenticatedController.getMe);
+apiAuthenticatedRouter.get("/users", apiAuthenticatedController.getTenantUsers);
+apiAuthenticatedRouter.get("/user/:userID", apiAuthenticatedController.getTenantUser);
+
+//tokens
+// apiAuthenticatedRouter.get("/tokens", apiAuthenticatedController.getTokens);
+
 
 module.exports = apiAuthenticatedRouter;
