6f51bfe032ce5d1ad6c2dd48fbc056bd1303b92b
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-04 10:35:49 +0100

    add test routes and authenticated router include

--- src/routes/api.v1.js
+++ src/routes/api.v1.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const apiController = require("../controller/api.controller");
+const apiAuthenticatedRouter = require("./api.authenticated.v1");
 
 const apiRouter = express.Router();
 
@@ -6,5 +8,11 @@ apiRouter.get("/", (req, res) => {
     res.json({ message: "Hello, World!" });
 
 });
+
+apiRouter.post("/test/new-tenant", apiController.TESTNewTenant);
+apiRouter.post("/test/upload-rule", apiController.TESTUploadRule);
+apiRouter.post("/test/new-token", apiController.TESTGenerateToken);
+
+apiRouter.use("", apiAuthenticatedRouter);
 
 module.exports = apiRouter;
