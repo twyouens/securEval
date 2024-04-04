83f89dded1b0c969715c36cf018b2158efadb5d8
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-04 10:35:02 +0100

    add web router, views and static

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const config = dotenv.config();
 dotenvExpand.expand(config);
+var path = require('path');
 
 const logger = require('./src/services/logger.service');
 app.use(logger);
 const apiRouter = require("./src/routes/api.v1");
 const connect = require('./src/services/db.service');
+const webRouter = require("./src/routes/web");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, process.env.TEMPLATE_DIR)); 
+app.set('view engine', 'ejs');
+
 app.use("/api/v1", apiRouter);
+app.use("/", webRouter);
+app.use("/static",express.static(path.join(__dirname, process.env.PUBLIC_DIR)));
 connect();
 app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: ${process.env.APP_URL}`)})
