27d47cad2a9deb08721ea19a4cdba5143a1fdea3
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-04 13:27:45 +0100

    add login password handler

--- src/controller/web.controller.js
+++ src/controller/web.controller.js
@@ -1,7 +1,28 @@
+const User = require('../models/user.model');
+
 function login (req, res) {
-    res.render("login");
+    res.render("login", {error: null});
 }
 
+async function loginPassword (req, res) {
+    const {username, password} = req.body;
+    const user = await User.findOne({username: username});
+    if(!user){
+        return res.status(404).render('login', {error: 'invalid'});
+    }
+    if(!await user.comparePassword(password)){
+        return res.status(404).render('login', {error: 'invalid'});
+    }
+    if(!user.isActive){
+        return res.status(404).render('login', {error: 'invalid'});
+    }
+    const token = user.createJWTToken();
+    res.cookie('token', token, {httpOnly: true, maxAge: parseInt(process.env.JWT_EXPIRY)*1000, path: '/'});
+    res.redirect('/');
+}
+    
+
 module.exports = {
-    login
+    login,
+    loginPassword
 };
