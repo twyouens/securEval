a836e3a99a9e743d967e9c3147496ec2cac27e67
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-04 13:28:09 +0100

    added login routes

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,6 +3,7 @@ const webController = require("../controller/web.controller");
 
 const webRouter = express.Router();
 
-webRouter.get("/login", webController.login); 
+webRouter.get("/login", webController.login);
+webRouter.post("/login", webController.loginPassword);
 
 module.exports = webRouter;
