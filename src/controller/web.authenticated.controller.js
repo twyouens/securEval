f2c9499d5aae868bce88412cbf9273b6337165d7
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-05 17:53:15 +0100

    added web authenticated controller

---
+++ src/controller/web.authenticated.controller.js
@@ -0,0 +1,31 @@
+const Rule = require('../models/rule.model');
+
+async function home (req, res) {
+    res.render("dashboard",{session: req.session});
+}
+
+async function logout (req, res) {
+    res.clearCookie('token');
+    res.redirect('/login');
+}
+
+async function rules (req, res) {
+    const tenantRules = await Rule.find({tenant: req.session.tenant._id}).select('name description version createdAt updatedAt targets');
+    res.render("rules",{session: req.session, rules: tenantRules});
+}
+
+async function manageRule (req, res) {
+    const ruleID = req.params.ruleID;
+    const rule = await Rule.findOne({_id: ruleID, tenant: req.session.tenant._id});
+    if(!rule){
+        return res.status(404).render('404');
+    }
+    res.render("rule",{session: req.session, rule: rule});
+}
+
+module.exports = {
+    home,
+    logout,
+    rules,
+    manageRule
+};
