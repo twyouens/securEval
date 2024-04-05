13070c29a2699ffb70000eccc8fbef22a61638eb
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-05 17:54:24 +0100

    add web authenticated router

---
+++ src/routes/web.authenticated.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const webAuthenticatedController = require("../controller/web.authenticated.controller");
+const {checkUserToken, addUserSessionData} = require("../middleware/auth");
+// const {moment} = require("../middleware/times");
+
+const webAuthenticatedRouter = express.Router();
+webAuthenticatedRouter.use(checkUserToken);
+webAuthenticatedRouter.use(addUserSessionData);
+// webAuthenticatedRouter.use(moment);
+
+webAuthenticatedRouter.get("/", webAuthenticatedController.home);
+webAuthenticatedRouter.get("/signout", webAuthenticatedController.logout);
+webAuthenticatedRouter.get("/rules", webAuthenticatedController.rules);
+webAuthenticatedRouter.get("/rule/:ruleID", webAuthenticatedController.manageRule);
+
+
+module.exports = webAuthenticatedRouter;
