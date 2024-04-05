bc9a714cc45ccf7c38ea6fc6fb2b0dd10c54c529
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-05 17:52:37 +0100

    moved static assets to fix bug with auth

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ app.use(logger);
 const apiRouter = require("./src/routes/api.v1");
 const connect = require('./src/services/db.service');
 const webRouter = require("./src/routes/web");
+const cookieParser = require('cookie-parser');
 const bp = require('body-parser');
 
 app.set('views', path.join(__dirname, process.env.TEMPLATE_DIR)); 
 app.set('view engine', 'ejs');
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1", apiRouter);
-app.use("/", webRouter);
 app.use("/static",express.static(path.join(__dirname, process.env.PUBLIC_DIR)));
+app.use("/", webRouter);
 connect();
 app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: ${process.env.APP_URL}`)})
