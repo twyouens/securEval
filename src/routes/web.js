a5c25e9acae47f9ac7804ea90e3f7c0c9cf7ac88
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-05 17:54:52 +0100

    update web router with use of authenticated router

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const webController = require("../controller/web.controller");
+const webAuthenticatedRouter = require("./web.authenticated");
 
 const webRouter = express.Router();
 
 webRouter.get("/login", webController.login);
 webRouter.post("/login", webController.loginPassword);
+
+webRouter.use("",webAuthenticatedRouter)
 
 module.exports = webRouter;
