960b4266ba00d2697d693573479e180ac118a510
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-10 14:23:40 +0100

    added ability for api auth to use user token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,17 +5,23 @@ const JWT = require('jsonwebtoken');
 
 async function checkAPIKey(req, res, next) {
     if(!req.headers.authorization){
+        if(req.cookies.token){
+            const token = req.cookies.token;
+            try{
+                var decoded = JWT.verify(token, process.env.JWT_SECRET);
+                req.token = decoded;
+                await addUserSessionData(req, res, next);
+                return next();
+            }catch(err){
+                req.log.warn({err: "Unauthorized Request", detail: "Invalid Token"}, "Unauthorized Request");
+                return res.status(401).json({message: 'Unauthorized'});
+            }
+        }
         req.log.warn({err: "Unauthorized Request", detail: "No Authorization Header"}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
     const token = (req.headers.authorization || "").replace(/^Bearer\s/,"");
-    const apiKey = await APIKey.findOne({key: token}, function(err, apiKey){
-        if(err){
-            req.log.error({err: err, detail: "Error finding API Key"}, "Internal Server Error");
-            res.status(500).json({message: 'Internal Server Error'});
-            return false;
-        }
-    });
+    const apiKey = await APIKey.findOne({key: token});
     if(!apiKey){
         req.log.warn({err: "Unauthorized Request", detail: "API Key not found"}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
@@ -28,6 +34,13 @@ async function checkAPIKey(req, res, next) {
         req.log.warn({err: "Unauthorized Request", detail: "API Key is not active", data: {apiKey: apiKey}}, "Unauthorized Request");
         return res.status(401).json({message: 'Unauthorized'});
     }
+    const tenant = await Tenant.findById(apiKey.tenant);
+    if(!tenant){
+        req.log.warn({err: "Unauthorized Request", detail: "Tenant not found", data: {apiKey: apiKey}}, "Unauthorized Request");
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+    req.session = {};
+    req.session['tenant'] = tenant;
     req.token = apiKey;
     next();
 }
