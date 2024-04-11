3135805edec0554af4852ad460b55963faec8bdf
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-11 11:04:30 +0100

    added handling to convert table row to form

--- public/js/manageRule.js
+++ public/js/manageRule.js
@@ -1,4 +1,6 @@
 var ruleModal;
+var ruleOutcomes = {};
+var ruleFacts = [];
 const initializeRuleModal = () => {
     ruleModal = new bootstrap.Modal(document.getElementById("ruleModal"), {});
 }
@@ -113,15 +115,89 @@ const renderFromTemplate = (templateID, data) => {
         }
         return ret;
     });
+    Handlebars.registerHelper('length', (context) => {
+        return context.length;
+    });
 
     const templateSrc = document.getElementById(templateID).innerHTML;
     const template = Handlebars.compile(templateSrc);
     return template(data);
 }
 
-const renderRuleConditions = () => {
-    let outcomes = JSON.parse($('#ruleOutcomesData').val());
-    console.log(outcomes);
-    const template = renderFromTemplate('ruleConditionsTemplate', {outcomes: outcomes});
+const renderRuleConditions = async () => {
+    const template = renderFromTemplate('ruleConditionsTemplate', {outcomes: ruleOutcomes});
     $('#ruleOutcomes').html(template);
+    addRowClickHandlers();
 }
+
+const loadOutcomes = async () => {
+    let ruleID = $('#ruleId').val();
+    $('#mainSpinner').show();
+    $.ajax({
+        url: '/api/v1/rule/'+ruleID,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data){
+            $('#mainSpinner').hide();
+            ruleOutcomes = data.outcomes;
+            ruleFacts = data.facts;
+            renderRuleConditions();
+        },
+        error: function(err){
+            $('#mainSpinner').hide();
+            console.log(err);
+        }
+    });
+}
+
+const addRowClickHandlers = () => {
+    $('.conditionsTable').each(function() {
+        $(this).on('click', 'tr', function(event) {
+            const $row = $(this);
+            if (!$(this).hasClass('form-converted')) {
+                convertRowToForm($row);
+                $(this).addClass('form-converted');
+            }
+        }
+        );
+    });
+    $('.conditionsTable').on('click', 'input', function(event) {
+        event.stopPropagation(); // Stop the click event from propagating to the row
+    });
+}
+
+const convertRowToForm = (row) => {
+    const $row = $(row);
+    const index = splitConditonRowIndex($row.data('index'));
+    const condition = ruleOutcomes[index[1]]['conditions'][index[2]][index[3]];
+
+    // // Replace text with input fields
+    const factCell = $row.find('td').eq(0);
+    factCell.html(`<select class='form-control fact-select'><option disabled>Select Fact</option><option value='${condition.fact}' selected>${condition.fact}</option></select>`);
+    const conditionOperator = renderFromTemplate('conditionOperatorSelectTemplate', {operator: condition.operator});
+    $row.find("td:nth-child(2)").html(conditionOperator);
+    $row.find("td:nth-child(3)").html(`<input type='text' value='${condition.value}' class='form-control'/>`);
+    const $editBtn = $row.find('.btnEditRow');
+    $editBtn.text('Save').off('click').on('click', function(event) {
+        // Prevent the row click event from firing when clicking the button
+        event.stopPropagation();
+        saveTask(index, $row.find("input:nth-child(1)").val(), $row.find("input:nth-child(2)").val(),$row.find("input:nth-child(3)").val());
+    });
+    factCell.find('.fact-select').one('click', function() {
+        populateFactOptions($(this), condition);
+    });
+}
+
+const splitConditonRowIndex = (index) => {
+    const parts = index.split('/');
+    const lastIndex = parts.length - 1;
+    parts[lastIndex] = parseInt(parts[lastIndex], 10);
+    return parts;
+}
+
+const populateFactOptions = (select,currentCondition) => {
+    select.find('option:not(:first)').remove();
+    ruleFacts.forEach(function(fact) {
+        select.append(new Option(fact.source+"-"+fact.name, fact.id, false, (fact.source+"-"+fact.name == currentCondition.fact)));
+    });
+};
