ecf8947be695efdeef8ca84bf5041d0cc54054d1
Thomas <t.w.youens2@newcastle.ac.uk>
2024-04-14 10:41:27 +0100

    removed test routes

--- src/routes/api.v1.js
+++ src/routes/api.v1.js
@@ -1,18 +1,8 @@
 const express = require("express");
-const apiController = require("../controller/api.controller");
 const apiAuthenticatedRouter = require("./api.authenticated.v1");
 
 const apiRouter = express.Router();
 
-apiRouter.get("/", (req, res) => {
-    res.json({ message: "Hello, World!" });
-
-});
-
-apiRouter.post("/test/new-tenant", apiController.TESTNewTenant);
-apiRouter.post("/test/upload-rule", apiController.TESTUploadRule);
-apiRouter.post("/test/new-token", apiController.TESTGenerateToken);
-
-apiRouter.use("", apiAuthenticatedRouter);
+apiRouter.use(apiAuthenticatedRouter);
 
 module.exports = apiRouter;
